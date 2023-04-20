b16ed52ba3a6f4637e12367ef90621fd31374d04
Nakayama Daiju <42.daiju@gmail.com>
2023-04-20 22:32:04 +0900

    Use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -2,19 +2,21 @@
 #
 # setup.py
 #
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name = "bitio",
-    packages = ["bitio"],
-    version = "0.2",
-    description = "Input/output utilities of a bit-basis file",
-    author = "Daiju Nakayama",
-    author_email = "42.daiju@gmail.com",
-    url = "https://github.com/hinohi/bitio",
-    classifiers = [
-        "Programming Language :: Python",
+    name="bitio",
+    packages=["bitio"],
+    version="0.2.0",
+    description="Input/output utilities of a bit-basis file",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Daiju Nakayama",
+    author_email="42.daiju@gmail.com",
+    url="https://github.com/hinohi/bitio",
+    classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
         "Topic :: Utilities",
